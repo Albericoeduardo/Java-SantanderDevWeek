7cb9e1b925abe8919f354b8e8561f6600a02275c
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-10-18 22:23:33 -0300

    Criação da pasta Controller e Exception Handler

--- src/main/java/me/dio/santanderdevweek2023/domain/repository/UserRepository.java
+++ src/main/java/me/dio/santanderdevweek2023/domain/repository/UserRepository.java
@@ -8,4 +8,5 @@ import me.dio.santanderdevweek2023.domain.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
+    boolean existsByAccountNumber(String accountNumber);
 }
